6b1aed0758c12550a9a2d4f6a3c6a5d251c30ec9
lehade <alexd89@list.ru>
2018-05-17 00:29:50 +0300

    hashmap fruits and etc

---
+++ src/main/java/ex0802.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ex0802 {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("арбуз", "ягода");
+        map.put("банан", "трава");
+        map.put("вишня", "ягода");
+        map.put("груша", "фрукт");
+        map.put("дыня", "овощ");
+        map.put("ежевика", "куст");
+        map.put("жень-шень", "корень");
+        map.put("земляника", "ягода");
+        map.put("ирис", "цветок");
+        map.put("картофель", "клубень");
+
+        for (Map.Entry<String, String> pair : map.entrySet())
+        {
+            String key = pair.getKey();                      //ключ
+            String value = pair.getValue();                  //значение
+            System.out.println(key + " - " + value);
+        }
+    }
+}
