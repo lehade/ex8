40081d59d32c70620372bcdb660ab391cfb7007a
lehade <alexey.denisenko@me.com>
2018-06-11 00:20:58 +0300

    var methods for lists

---
+++ src/main/java/ex0811.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ex0811 {
+
+    public static List getListForGet() {
+        //напишите тут ваш код
+        return new ArrayList<String>();
+    }
+
+    public static List getListForSet() {
+        //напишите тут ваш код
+        return new ArrayList<String>();
+    }
+
+    public static List getListForAddOrInsert() {
+        //напишите тут ваш код
+        return new LinkedList<String>();
+    }
+
+    public static List getListForRemove() {
+        //напишите тут ваш код
+        return new LinkedList<String>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
