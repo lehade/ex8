a2e9503da831d44c52c64d116d99e6db1fd25d75
lehade <alexey.denisenko@me.com>
2018-06-10 22:34:08 +0300

    arraylist + linkedlist

---
+++ src/main/java/ex0807.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ex0807 {
+
+    public static Object createArrayList() {
+        //напишите тут ваш код
+        List<String> arraylist = new ArrayList<String>();
+        arraylist.add("Mama");
+        arraylist.add("Mila");
+        arraylist.add("Ramu");
+        return arraylist;
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+        List<String> linkedlist = new LinkedList<String>();
+        linkedlist.add("Mama");
+        linkedlist.add("Mila");
+        linkedlist.add("Ramu");
+        return linkedlist;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
