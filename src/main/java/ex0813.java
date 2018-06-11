092927a267e7be5d683d3d9cccc0d91f95024859
lehade <alexey.denisenko@me.com>
2018-06-11 22:20:58 +0300

    hashset Л20 adds

---
+++ src/main/java/ex0813.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ex0813 {
+
+        public static Set<String> createSet() {
+            //напишите тут ваш код
+            Set<String> list = new HashSet<String>();
+            list.add("Лфф");
+            list.add("Лss");
+            list.add("Лff");
+            list.add("Лdd");
+            list.add("Лgg");
+            list.add("Лhh");
+            list.add("Лjj");
+            list.add("Лkk");
+            list.add("Лll");
+            list.add("Лzz");
+            list.add("Лф");
+            list.add("Лs");
+            list.add("Лf");
+            list.add("Лd");
+            list.add("Лg");
+            list.add("Лh");
+            list.add("Лj");
+            list.add("Лk");
+            list.add("Лl");
+            list.add("Лz");
+
+            return list;
+
+        }
+
+        public static void main(String[] args) {
+
+        }
+
+}
